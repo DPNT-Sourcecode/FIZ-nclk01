79d64ab2dbfbee78653e8552380d56c86b3ebda3
sbx_user1059 <sbx_user1059@ip-10-128-129-168.eu-west-2.compute.internal>
2019-01-21 21:26:15 +0000

    Mon Jan 21 21:26:15 UTC 2019

--- src/BeFaster.App/Solutions/FIZ/FizzBuzzDeluxHandler.cs
+++ src/BeFaster.App/Solutions/FIZ/FizzBuzzDeluxHandler.cs
@@ -7,19 +7,19 @@ namespace BeFaster.App.Solutions.FIZ
         public override string Handle(int number)
         {
             if (
-                (
+                (   // fizz buzz
                     (Convert.ToString(number).IndexOf('3') >= 0 && Convert.ToString(number).IndexOf('5') >= 0) ||
                     (Convert.ToString(number).IndexOf('3') >= 0 && (number % 5) == 0) ||
                     (Convert.ToString(number).IndexOf('5') >= 0 && (number % 3) == 0)
                 ) && 
-                (
+                (   // delux
                     (number % 11 == 0) ||
                     (number % 111 == 0) ||
                     (number % 1111 == 0)
                 )
                )
             {
-                return "deluxe";
+                return "fizz buzz deluxe";
             }
             else
             {
@@ -29,4 +29,5 @@ namespace BeFaster.App.Solutions.FIZ
     }
 
 }
+
 
