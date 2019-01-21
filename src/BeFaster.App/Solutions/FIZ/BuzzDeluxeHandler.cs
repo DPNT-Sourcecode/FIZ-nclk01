a6fa707b9e8d2f300f9fd12fc78af71150cc388d
sbx_user1059 <sbx_user1059@ip-10-128-129-168.eu-west-2.compute.internal>
2019-01-21 21:45:18 +0000

    Mon Jan 21 21:45:18 UTC 2019

---
+++ src/BeFaster.App/Solutions/FIZ/BuzzDeluxeHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace BeFaster.App.Solutions.FIZ
+{
+    public class BuzzDeluxeHandler : AbstractHandler
+    {
+        public override string Handle(int number)
+        {
+            if (
+                (   // delux
+                    (number % 11 == 0) ||
+                    (number % 111 == 0) ||
+                    (number % 1111 == 0)
+                )
+                &&
+                (   // buzz
+                    (number % 5 == 0)
+                )
+               )
+            {
+                return "fizz deluxe";
+            }
+            else
+            {
+                return base.Handle(number);
+            }
+        }
+    }
+
+}
---
+++ src/BeFaster.App/Solutions/FIZ/FizzDeluxeHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace BeFaster.App.Solutions.FIZ
+{
+    public class FizzDeluxeHandler : AbstractHandler
+    {
+        public override string Handle(int number)
+        {
+            if (
+                (   // delux
+                    (number % 11 == 0) ||
+                    (number % 111 == 0) ||
+                    (number % 1111 == 0)
+                )
+                &&
+                (   // fizz
+                    (number % 3 == 0)
+                )
+               )
+            {
+                return "fizz deluxe";
+            }
+            else
+            {
+                return base.Handle(number);
+            }
+        }
+    }
+
+}
