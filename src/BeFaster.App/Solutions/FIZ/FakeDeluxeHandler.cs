df9d4851f81a64407a1adab5b8c6d45b9ab1d43d
sbx_user1059 <sbx_user1059@ip-10-128-129-168.eu-west-2.compute.internal>
2019-01-21 22:05:09 +0000

    Mon Jan 21 22:05:09 UTC 2019

---
+++ src/BeFaster.App/Solutions/FIZ/FakeDeluxeHandler.cs
@@ -0,0 +1,26 @@
+﻿namespace BeFaster.App.Solutions.FIZ
+{
+    public class FakeDeluxeHandler : AbstractHandler
+    {
+        public override string Handle(int number)
+        {
+            if (
+                (
+                    (number % 11 == 0) ||
+                    (number % 111 == 0) ||
+                    (number % 1111 == 0)
+                )
+                &&
+                (number % 2 == 1)
+                )
+            {
+                return "fake deluxe";
+            }
+            else
+            {
+                return base.Handle(number);
+            }
+        }
+    }
+
+}
