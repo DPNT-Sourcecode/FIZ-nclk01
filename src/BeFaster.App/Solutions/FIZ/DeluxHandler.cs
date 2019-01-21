66bd52d7fd9826dfa42506f24e4cfd3074fd9ed0
sbx_user1059 <sbx_user1059@ip-10-128-129-168.eu-west-2.compute.internal>
2019-01-21 21:14:14 +0000

    Mon Jan 21 21:14:14 UTC 2019

---
+++ src/BeFaster.App/Solutions/FIZ/DeluxHandler.cs
@@ -0,0 +1,18 @@
+﻿namespace BeFaster.App.Solutions.FIZ
+{
+    public class DeluxHandler : AbstractHandler
+    {
+        public override string Handle(int number)
+        {
+            if (number % 11 == 0)
+            {
+                return "deluxe";
+            }
+            else
+            {
+                return base.Handle(number);
+            }
+        }
+    }
+
+}
