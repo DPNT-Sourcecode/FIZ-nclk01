db4634b76c73d454e206869ea97941d9d9fbd0cd
sbx_user1059 <sbx_user1059@ip-10-128-129-168.eu-west-2.compute.internal>
2019-01-21 20:36:15 +0000

    Mon Jan 21 20:36:15 UTC 2019

--- src/BeFaster.App/Solutions/FIZ/FizzContainerHandler.cs
+++ src/BeFaster.App/Solutions/FIZ/FizzContainerHandler.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace BeFaster.App.Solutions.FIZ
 {
-    class FizzContainerHandler
+    public class FizzContainerHandler : AbstractHandler
     {
+        public override string Handle(int number)
+        {
+            if (Convert.ToString(number).IndexOf('3') > 0)
+            {
+                return "fizz";
+            }
+            else
+            {
+                return base.Handle(number);
+            }
+        }
     }
 }
+
