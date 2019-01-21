934a569300faf74f59ff17385bca3b2c137efea3
sbx_user1059 <sbx_user1059@ip-10-128-129-168.eu-west-2.compute.internal>
2019-01-21 19:17:29 +0000

    Mon Jan 21 19:17:29 UTC 2019

--- src/BeFaster.App/Solutions/HLO/HelloSolution.cs
+++ src/BeFaster.App/Solutions/HLO/HelloSolution.cs
@@ -6,7 +6,11 @@ namespace BeFaster.App.Solutions.HLO
     {
         public static string Hello(string friendName)
         {
+            // TODO:    Check with customers, why we are passing friendName parameter to this method,
+            //          and what message they want us to return.
+
             return "Hello World";
         }
     }
 }
+
