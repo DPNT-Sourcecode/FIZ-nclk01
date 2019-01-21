5c9c6b5d697d969176e3b8ab9044d05b5c12d8c5
sbx_user1059 <sbx_user1059@ip-10-128-129-168.eu-west-2.compute.internal>
2019-01-21 20:39:14 +0000

    Mon Jan 21 20:39:14 UTC 2019

---
+++ src/BeFaster.App/Solutions/FIZ/BuzzContainerHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BeFaster.App.Solutions.FIZ
+{
+    public class BuzzContainerHandler : AbstractHandler
+    {
+        public override string Handle(int number)
+        {
+            if (Convert.ToString(number).IndexOf('5') > 0)
+            {
+                return "buzz";
+            }
+            else
+            {
+                return base.Handle(number);
+            }
+        }
+    }
+}
--- src/BeFaster.App/Solutions/FIZ/FizzBuzzSolution.cs
+++ src/BeFaster.App/Solutions/FIZ/FizzBuzzSolution.cs
@@ -5,11 +5,18 @@
         public static string FizzBuzz(int number)
         {
             var fizzHandler = new FizzHandler();
+            var fizzNumberContainerHandler = new FizzNumberContainerHandler();
             var buzzHandler = new BuzzHandler();
             var fizzBuzzHandler = new FizzBuzzHandler();
+            var buzzContainerHandler = new BuzzContainerHandler();
             var numberHandler = new NumberHandler();
 
-            fizzBuzzHandler.setNext(fizzHandler).setNext(buzzHandler).setNext(numberHandler);
+            fizzBuzzHandler
+                .setNext(fizzHandler)
+                .setNext(fizzNumberContainerHandler)
+                .setNext(buzzHandler)
+                .setNext(buzzContainerHandler)
+                .setNext(numberHandler);
 
             string result = fizzBuzzHandler.Handle(number);
 
@@ -18,3 +25,4 @@
     }
 
 }
+
