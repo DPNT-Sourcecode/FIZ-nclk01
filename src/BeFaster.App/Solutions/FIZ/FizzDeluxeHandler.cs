fabf8d8748aadcb0e4376ee16cbcb05745c477a6
sbx_user1060 <sbx_user1060@ip-10-129-115-144.eu-west-2.compute.internal>
2019-01-22 00:30:50 +0000

    Tue Jan 22 00:30:50 UTC 2019

--- src/BeFaster.App/Solutions/FIZ/FizzDeluxeHandler.cs
+++ src/BeFaster.App/Solutions/FIZ/FizzDeluxeHandler.cs
@@ -8,8 +8,8 @@ namespace BeFaster.App.Solutions.FIZ
         {
             if (    // deluxe
                 (
-                    (number % 3 == 0) && (Convert.ToString(number).IndexOf('3') >= 0) ||
-                    (number % 5 == 0) && (Convert.ToString(number).IndexOf('5') >= 0)
+                    ((number % 3 == 0) && (Convert.ToString(number).IndexOf('3') >= 0)) ||
+                    ((number % 5 == 0) && (Convert.ToString(number).IndexOf('5') >= 0))
                 )
                 &&
                 (   // fizz
@@ -27,4 +27,5 @@ namespace BeFaster.App.Solutions.FIZ
     }
 
 }
+
 
