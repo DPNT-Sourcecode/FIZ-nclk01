2404da67daa8a1110b2d05a8209c76bc6dfa3db8
sbx_user1060 <sbx_user1060@ip-10-129-115-144.eu-west-2.compute.internal>
2019-01-22 00:15:50 +0000

    Tue Jan 22 00:15:50 UTC 2019

--- src/BeFaster.App/Solutions/FIZ/FakeDeluxeHandler.cs
+++ src/BeFaster.App/Solutions/FIZ/FakeDeluxeHandler.cs
@@ -1,4 +1,6 @@
-﻿namespace BeFaster.App.Solutions.FIZ
+﻿using System;
+
+namespace BeFaster.App.Solutions.FIZ
 {
     public class FakeDeluxeHandler : AbstractHandler
     {
@@ -6,9 +8,8 @@
         {
             if (
                 (
-                    (number % 11 == 0) ||
-                    (number % 111 == 0) ||
-                    (number % 1111 == 0)
+                    (number % 3 == 0) && (Convert.ToString(number).IndexOf('3') >= 0) ||
+                    (number % 5 == 0) && (Convert.ToString(number).IndexOf('5') >= 0)
                 )
                 &&
                 (number % 2 == 1)
@@ -24,3 +25,4 @@
     }
 
 }
+
